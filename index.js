901ca6834b81f1f9d039bbe50f250fc7080fc2c3
tendeanclaudio <69841294+tendeanclaudio@users.noreply.github.com>
2020-11-26 22:09:04 +0800

    Final Project

---
+++ index.js
@@ -0,0 +1,11 @@
+<!-- html -->
+<h1 id="title">Claudio Tendean</h1>
+<p>JavaScript</p>
+<h1 class="subtitle">Claudio Rezy Tendean</h1>
+let semuaTagH1 = document.getElementsByTagName("h1");
+
+console.log(semuaTagH1); // Output: HTMLCollection(2) [h1#title, h1.subtitle]
+console.log(semuaTagH1[0]); // Output: <h1 id="title">Hello, World!</h1>
+console.log(semuaTagH1[1]); // Output: <h1 class="subtitle">Mari Belajar JavaScript</h1>
+
+
